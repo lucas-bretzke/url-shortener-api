280551927b03b485e61a18761df79e5c22771c07
Lucas Bretzke <lucasbretzke@gmail.com>
2024-08-04 08:01:43 -0300

    Starting change from Railway to Render

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const prisma = new PrismaClient()
 
 const app = express()
 const port = Number(process.env.PORT || 8080)
-const ipAddress = '192.168.0.4'
+const ipAddress = '0.0.0.0'
 
 app.use(express.json())
 app.use(express.raw({ type: 'application/vnd.custom-type' }))
@@ -27,32 +27,32 @@ app.use('/user', userResolvers)
 // Rotas para funcionalidades de autenticação
 app.use('/auth', auth)
 
-app.get('/:code', async (req, res) => {
-  const code = req.params.code
-  const fullUrl = `bretz.up.railway.app/${code}`
+// app.get('/:code', async (req, res) => {
+//   const code = req.params.code
+//   const fullUrl = `bretz.up.railway.app/${code}`
 
-  try {
-    const link = await prisma.link.findFirst({
-      where: { short_url: fullUrl }
-    })
+//   try {
+//     const link = await prisma.link.findFirst({
+//       where: { short_url: fullUrl }
+//     })
 
-    if (!link) return res.status(404).send('URL not found')
+//     if (!link) return res.status(404).send('URL not found')
 
-    await prisma.link.update({
-      where: { link_id: link.link_id },
-      data: {
-        access_count: {
-          increment: 1
-        }
-      }
-    })
+//     await prisma.link.update({
+//       where: { link_id: link.link_id },
+//       data: {
+//         access_count: {
+//           increment: 1
+//         }
+//       }
+//     })
 
-    res.redirect(link.original_url)
-  } catch (error) {
-    console.log('get error', error)
-    res.status(500).send('Internal Server Error')
-  }
-})
+//     res.redirect(link.original_url)
+//   } catch (error) {
+//     console.log('get error', error)
+//     res.status(500).send('Internal Server Error')
+//   }
+// })
 
 app.get('/', async (req, res) => {
   try {
@@ -66,5 +66,5 @@ app.get('/', async (req, res) => {
  * Server start.
  */
 app.listen(port, ipAddress, () => {
-  console.log(`Server is running on http://192.168.0.4:${port}`)
+  console.log(`Server is running on http://localhost:${port}`)
 })
